f751a9e3959c989eb353aa3e1f2558e37566c234
Jostein Berre Eliassen <spam@jostein.be>
2018-11-23 15:18:38 +0100

    added dossequiz template
    so i can work on it from home,
    if want to

---
+++ dosse_quiz/main.lua
@@ -0,0 +1,12 @@
+--
+-- DosseQuiz
+--
+-- Work in progress...
+-- Trying to implement this in lua using the Lõve game engine..
+--
+
+local socket = require("socket")
+
+function love.draw()
+  love.graphics.print("Hello World!", 400, 300)
+end
