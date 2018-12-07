d6d531c31164a1b7a053ae17e24c2988c5769e7d
Jostein Berre Eliassen <spam@jostein.be>
2018-12-07 12:24:46 +0100

    config editor initial commit

---
+++ software/config_editor/server/config_server.rb
@@ -0,0 +1,22 @@
+require "bundler/inline"
+
+require "json"
+require "sinatra"
+
+# gemfile do # this uses bundler to install external gems
+#   source "https://rubygems.org"
+#   gem "sinatra", require: true
+# end
+
+FILE_NAME = ARGV[0]
+
+put "/config" do
+  config = JSON.parse(request.body.read)
+  File.write FILE_NAME, config.to_json
+  200
+end
+
+get "/config" do
+  content_type :json
+  File.read FILE_NAME
+end
